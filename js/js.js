88d4c8eb5289164ca3e8a14ea4aa72821ac1a3fb
squalvj <squalvj@gmail.com>
2017-10-04 06:53:16 +0700

    add email address

--- js/js.js
+++ js/js.js
@@ -3,9 +3,22 @@ particlesJS.load('particles-js', 'https://cdn.rawgit.com/squalvj/resume/gh-pages
 });
 var temporary = $(".c1")
 
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 
 jQuery(document).ready(function($) {
-
+	var foo = getParameterByName('status');
+	if (foo == 'berhasil'){
+		alert("Pesan anda berhasil terkirim");
+	}
 	$(".link-ex").hover(function() {
 		var e = $(this).siblings('.tag-ex').children('div')
 	    e.velocity({"textShadowBlur": 20});
